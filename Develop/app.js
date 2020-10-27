0e76f699e63c04e3196064898cc51f6e12d713a8
Timothy Forbes <timothy.forbes259@gmail.com>
2020-10-27 12:18:20 +1030

    automated selector loop which is successful for building team members

--- Develop/app.js
+++ Develop/app.js
@@ -9,9 +9,11 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choices = require("inquirer/lib/objects/choices");
 
 
 // Write code to use inquirer to gather information about the development team members,
+
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 //1. inquirer.prompt to call questions based on the class. 
@@ -39,16 +41,117 @@ const projectLeader = () => {
 
     ]) .then( data => {
         const projectLeader = new Manager(data.name, data.id, data.email, data.officeNumber);
+        
         console.log(projectLeader);
+
+        memberSelection();
+
     }) .catch(error => {
         console.log(error);
         //invalid for number input
-        
+
     })
 }
+// selector for other team members to trigger team member functions
+let otherMembersPrompt = 
+    {
+        name: "otherMembers",
+        message: "Are there Any other members in your team?",
+        type: "list",
+        choices: ['Intern','Engineer','Finished'],
+
+    }
+// function to trigger additional team member information
+function memberSelection () {
+    inquirer.prompt(otherMembersPrompt).then((answers) => {
+        if (answers.otherMembers === 'Engineer') {
+            engineerTeamMember ();
+        } else if (answers.otherMembers === 'Intern') {
+            internTeamMember ();
+        } else if (answers.otherMembers === 'Finished') {
+            console.log("Formatting team members");
+        }
+    });
+}
+// seperate function to determine type to call employee type.
+
+function engineerTeamMember() {
+    // Inquirer.prompt for Engineer class
+
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is your Name?",
+            type: "input",
+        },
+        {
+            name: "id",
+            message: "What is your Employee's ID Number (must be a Number Input)?",
+            type: "number",
+        },
+        {
+            name: "email",
+            message: "What is your Email Address for Contact?",
+            type: "input"
+        },
+        {
+            name: "github",
+            message: "What is your github username?",
+             type: "input",
+        },
+        
+        ]) .then( data => {
+                const engineer = new Engineer(data.name, data.id, data.email, data.github);
+                console.log(engineer);
+                // calling for additional members
+                memberSelection();
+        }) .catch(error => {
+                console.log(error);
+        
+            })
+
+          } 
+// function for intern functions
+function internTeamMember () {
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is your Name?",
+            type: "input",
+        },
+        {
+            name: "id",
+            message: "What is your Employee's ID Number (must be a Number Input)?",
+            type: "number",
+        },
+        {
+            name: "email",
+            message: "What is your Email Address for Contact?",
+            type: "input"
+        },
+        {
+            name: "school",
+            message: "Which school are you from?",
+                type: "input",
+        },
+
+        ]) .then( data => {
+                const intern= new Intern(data.name, data.id, data.email, data.school);
+                console.log(intern);
+                memberSelection()
+        }) .catch(error => {
+                console.log(error);
+    
+        })
+
+} 
+// use inquirer.prompt inbuilt function capacity to work the answers.
+
+// function to create data for an intern
 
 //2.. Using information given by the inquier log the data to the console
 projectLeader();
+
 
 //3.. Ask if any other uses wish to be used, if selected trigger a function to call inquirer for thre required data.
 
